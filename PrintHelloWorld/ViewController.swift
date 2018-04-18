9efe5c56a32d789211490463f1bbbb316b729602
Almostafa Fadel Barakat <34185666+AlmostafaFadelBarakat@users.noreply.github.com>
2018-04-18 22:20:50 +0200

    Update ViewController.swift

--- PrintHelloWorld/ViewController.swift
+++ PrintHelloWorld/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("HelloWorld!")
+        print("HelloW!")
+        func mon() {
+            print(1)
     }
 
     override func didReceiveMemoryWarning() {
